058639f978e78822f471bff28e45e87b0e3a4359
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2022-02-20 14:57:13 +0100

    Kinézetek kész

--- src/main/java/com/example/muzeumfrontendjavafx/controllers/ListazasController.java
+++ src/main/java/com/example/muzeumfrontendjavafx/controllers/ListazasController.java
@@ -1,9 +1,9 @@
-package com.example.muzeumfrontendjavafx;
+package com.example.muzeumfrontendjavafx.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ListazasController {
     @FXML
     private Label welcomeText;
 
